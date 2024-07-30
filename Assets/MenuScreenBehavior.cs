2c6b09a2986997a452e5b505f78814d677e29d4d
EchoZeppelin <jawshbawx@gmail.com>
2024-07-30 13:14:09 -0700

    Event System Fix Maybe
    Event system now invisible. Separate event system might be running in the engine script

--- Assets/MenuScreenBehavior.cs
+++ Assets/MenuScreenBehavior.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Events;
+// using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using TMPro;
 
