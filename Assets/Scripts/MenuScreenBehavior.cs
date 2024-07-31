79675cf4f766ac2a7a76ef5c3b0aa6ed717b4c30
EchoZeppelin <jawshbawx@gmail.com>
2024-07-30 22:19:44 -0700

    Update
    Still Broken

--- Assets/Scripts/MenuScreenBehavior.cs
+++ Assets/Scripts/MenuScreenBehavior.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-// using UnityEngine.Events;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using TMPro;
 
@@ -31,6 +31,7 @@ public class MenuScreenBehavior : MonoBehaviour
             this.sceneName = sceneName;
             this.isTimed = is_timed;
             this.isAdditive = is_additive;
+            Debug.Log("screenValues Started");
         }
     }
 
@@ -65,11 +66,13 @@ public class MenuScreenBehavior : MonoBehaviour
         timer = 0.0f;
         isCurrentScreenTimed = screen_dict[screen].isTimed;
         currentScreen = screen; // Update currentScreen here
-        Debug.Log("Current screen is now: " + currentScreen);
+        Debug.Log("setCurrentScreen is now: " + currentScreen);
     }
 
     screenValues MakeScreenValueStruct(string sceneName, bool is_timed, bool is_additive)
     {
+        Debug.Log("MakeScreenValueStruct Started");
+        
         /*
         This is a helper function which takes in a scene name and a bool to create a screenValues
         struct. This is done because it makes the objective of creating a screenValues struct 
@@ -81,6 +84,7 @@ public class MenuScreenBehavior : MonoBehaviour
         Returns:
             screen_values (screenValues) : a screenValues struct with fields populated using sceneName, is_timed, and is_additive
         */
+
         screenValues screen_values = new screenValues(sceneName, is_timed, is_additive);
         return screen_values;
     }
@@ -88,31 +92,32 @@ public class MenuScreenBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Void Start");
         /*
         This function is called only once by the Unity engine before the first frame update. In this
         implementation, it initializes certain variables before the start of the start of the menu-scene
         Unity Scene. This function takes no arguments for input and returns nothing.
         */
+
         // initializing a dictionary mapping each screen to a struct containing its values
         screenToValuesDict = new Dictionary<string, screenValues>() {
             {"main", MakeScreenValueStruct("mainScreen", false, false)},
             {"splash",  MakeScreenValueStruct("splashScreen", true, true)},
             {"loading", MakeScreenValueStruct("loadingScreen", true, true)},
             {"game", MakeScreenValueStruct("gameScreen", true, true)},
-            // {"options", MakeScreenValueStruct("OptionsMenuScreen", false, false)}, // Menu screens to be implemented later
-            // {"exit", MakeScreenValueStruct("ExitScreen", true, false)}
+            // {"options", MakeScreenValueStruct("optionsMenuScreen", false, false)}, // Menu screens to be implemented later
+            // {"exit", MakeScreenValueStruct("exitScreen", true, false)}
         };
 
         currentScreen = "main";
         setCurrentScreen(screenToValuesDict, currentScreen); // starts the main scene and sets values
-        Debug.Log("Starting main screen");
+        Debug.Log("currentScreen = mainScreen");
 
         // Button implementation to follow
 
         // exitButton.onClick.AddListener(() => setCurrentScreen(screenToValuesDict, "exit"));
         // optionsButton.onClick.AddListener(() => setCurrentScreen(screenToValuesDict, "options"));
         // optionsBackButton.onClick.AddListener(() => setCurrentScreen(screenToValuesDict, "main"));
-        // skipLoadingButton.onClick.AddListener(() => setCurrentScreen(screenToValuesDict, "main"));
     }
 
     // Update is called once per frame
@@ -123,18 +128,20 @@ public class MenuScreenBehavior : MonoBehaviour
         it is used to time the screen in the scene and change them using the display time
         variables defined within this script. This function takes no arguments and returns nothing.
         */
+        Debug.Log("Void Update");
 
         if (isCurrentScreenTimed)
         {
             // only updates timer on timed screens
             timer += Time.deltaTime;
-            Debug.Log("Timer: " + timer);
+            Debug.Log("Timer Started ");
         }
-
+        
         // dynamically changing the visible screen based on the timer
         switch (currentScreen)
         {
             case "main":
+            Debug.Log("Switch Started");
                 if (timer >= splashScreenSeconds)
                 {
                     setCurrentScreen(screenToValuesDict, "splash");
@@ -175,3 +182,4 @@ public class MenuScreenBehavior : MonoBehaviour
         }
     }
 }
+
